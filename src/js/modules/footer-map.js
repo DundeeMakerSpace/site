d228db8b47ad4e91e8eb79819ef0eb0f31a7bcf3
Grant Richmond <mail@grant.codes>
2016-11-29 12:06:43 +0000

    switch map tiles to https

--- src/js/modules/footer-map.js
+++ src/js/modules/footer-map.js
@@ -11,8 +11,8 @@ export default function() {
         // zoomControl: false
     }).setView([lat, lon], 16);
 
-    const CartoDB_DarkMatter = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    const CartoDB_DarkMatter = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://cartodb.com/attributions">CartoDB</a>',
         subdomains: 'abcd',
         maxZoom: 19
     });
